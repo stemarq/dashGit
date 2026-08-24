92ad8d65f882aa9706293a59e2328b59be869a60
Stephany Marques <ster.mark1@gmail.com>
2026-08-24 01:19:48 -0300

    test(commits): cria o modulo test_commits

---
+++ tests/test_commits.py
@@ -0,0 +1,7 @@
+"""Testes das metricas de commits, com um cache SQLite sintetico."""
+
+import os
+import tempfile
+from datetime import datetime, timedelta, timezone
+
+os.environ.setdefault("GITLAB_TOKEN", "test-token")
